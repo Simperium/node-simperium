39fa6234a1569817fee52eac4e577d868b136229
Beau Collins <beaucollins@gmail.com>
2015-06-12 12:45:51 -0700

    Updating sample using callback

--- sample/simplenote.js
+++ sample/simplenote.js
@@ -6,36 +6,35 @@ client.connect();
 
 client.users.on('authorize', function(user){
 
-  var notes = client.bucket('note', user)
-    , tags = client.bucket('tag', user);
+  var notes = client.bucket('note', user),
+      tags = client.bucket('tag', user);
 
   notes.on('index', function(){
 
-    var now = (new Date()).getTime()
-      , note = {
-        content: "Hola mundo!"
-        , systemTags:[]
-        , tags:[]
-        , creationDate:now
-        , modificationDate:now
-        , publishURL:""
-        , shareURL:""
-        , deleted: false
-      };
+    var now = (new Date()).getTime(),
+        note = {
+          content: "Hola mundo!",
+          systemTags:[],
+          tags:[],
+          creationDate:now,
+          modificationDate:now,
+          publishURL:"",
+          shareURL:"",
+          deleted: false
+        };
 
-    var id = notes.add(note);
+    notes.add(note, function(err, id, object) {
+      // if (err) throw err;
+      setTimeout(function(){
+        console.log('update!', id);
+        note.content += "\n\nEl Fin";
+        notes.update(id, note);
+      }, 2000);
+    });
 
-    setTimeout(function(){
-      console.log('update!', id);
-      note.content += "\n\nEl Fin";
-      notes.update(id, note)
-    }, 2000);
-
-  })
+  });
 
 });
-
-console.log("Authorizing");
 
 client.users.authorize(
   process.env.SIMPLENOTE_USERNAME,
@@ -50,9 +49,10 @@ client.users.authorize(
 client.on('send', function(data) {
   console.warn(" => ", data.slice(0, 200));
 });
+
 client.on('message', function(message) {
-  console.warn(" <= ", message.slice(0, 150));
-})
+  console.warn(" <= ", message.slice(0, 80));
+});
 
 client.on('reconnect', function(attempt){
   console.warn("Attempting reconnection", attempt);
