0c48178f7ee67b434656f9959557afff1775b2ad
Beau Collins <beaucollins@gmail.com>
2015-11-25 00:33:22 -0800

    Using setImmediate to escape promise error handling

--- test/simperium/channel_test.js
+++ test/simperium/channel_test.js
@@ -377,10 +377,12 @@ describe( 'Channel', function() {
 			var cv = 'abcdefg';
 
 			channel.once( 'send', function( data ) {
-				var message = parseMessage( data );
-				assert.equal( 'cv', message.command );
-				assert.equal( cv, message.data );
-				done();
+				setImmediate(function() {
+					var message = parseMessage( data );
+					assert.equal( 'cv', message.command );
+					assert.equal( cv, message.data );
+					done();
+				})
 			} );
 
 			store.setChangeVersion( cv ).then( function() {
