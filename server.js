acdb0aa3ae0d99dbcb8381d4b288c9fa0b9b6a3b
Beau Collins <beaucollins@gmail.com>
2015-12-05 10:35:58 -0800

    add server entry point

---
+++ server.js
@@ -0,0 +1,7 @@
+var Connection = require( './lib/simperium/server/connection' )
+var Channel = require( './lib/simperium/server/channel' )
+
+module.exports = {
+	Connection: Connection.default,
+	Channel: Channel.default
+}
