471233e5754a31bebba3450b41145c0c6d3260aa
Robert Collins <beaucollins@gmail.com>
2015-12-15 13:40:19 -0800

    Add tests for imports

---
+++ test/simperium/simperium_test.js
@@ -0,0 +1,10 @@
+import { util } from '../../src/simperium'
+import assert from 'assert'
+
+describe( 'Simperium', () => {
+	it( 'should export utils', () => {
+		assert( util )
+		assert( util.change )
+		assert.equal( typeof util.change.diff, 'function' )
+	} )
+} )
