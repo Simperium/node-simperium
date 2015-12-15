e73f0f5320eaa3dd308f7f62bbf79c39fa2cbef6
Robert Collins <beaucollins@gmail.com>
2015-12-15 13:36:43 -0800

    Fix import statement for util

--- src/simperium/index.js
+++ src/simperium/index.js
@@ -1,7 +1,7 @@
 import User from './user'
 import Client from './client'
 import Auth from './auth'
-import util from './util'
+import * as util from './util'
 
 export default function( appId, token, options ) {
 	return new Client( appId, token, options );
