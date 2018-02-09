4ad56fba649bb5368e92357ff096a69d15a09245
Robert Collins <beau@collins.pub>
2018-02-09 12:13:47 -0800

    change import to just https import
    Organizes utility classes to declutter Client module

--- src/simperium/auth.js
+++ src/simperium/auth.js
@@ -1,16 +1,22 @@
 // @flow
 import events from 'events'
-import https from 'https'
+import { request } from 'https'
 import url from 'url'
 
 // @flow
-type User = {};
+type User = {
+	options: {},
+	access_token: string,
+};
 
-const fromJSON = ( json: string ) => {
-	const data = JSON.parse( json );
+const fromJSON = ( json: string ): User => {
+	const data: {} = JSON.parse( json );
+	if ( ! data.access_token && typeof data.access_token !== 'string' ) {
+		throw new Error( 'access_token not present' );
+	}
 	return {
 		options: data,
-		access_token: data.access_token
+		access_token: new String( data.access_token ).toString()
 	};
 };
 
@@ -27,16 +33,32 @@ export class AuthError extends Error {
 	}
 }
 
+/**
+ * Client for creating and authenticating Simperium.com user accounts.
+ */
 export class Auth extends EventEmitter {
 	appId: string
 	appSecret: string
 
+	/**
+	 * Creates an instance of the Auth client
+	 *
+	 * @param {string} appId - Simperium.com application ID
+	 * @param {string} appSecret - Simperium.com application secret
+	 */
 	constructor( appId: string, appSecret: string ) {
 		super();
 		this.appId = appId;
 		this.appSecret = appSecret;
 	}
 
+	/**
+	 * Authorizes a user account with username and password
+	 *
+	 * @param {string} username account username
+	 * @param {string} password account password
+	 * @returns {Promise<User>} user account data
+	 */
 	authorize( username: string, password: string ) {
 		const body = JSON.stringify( { username: username, password: password } );
 		return this.request( 'authorize/', body );
@@ -62,7 +84,7 @@ export class Auth extends EventEmitter {
 
 	request( endpoint: string, body: string ): Promise<User> {
 		return new Promise( ( resolve, reject ) => {
-			const req = https.request( this.getUrlOptions( endpoint ), ( res ) => {
+			const req = request( this.getUrlOptions( endpoint ), ( res ) => {
 				let responseData = '';
 
 				res.on( 'data', ( data ) => {
