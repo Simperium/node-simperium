75836b130a4d3e3d64276aab6b32bfec83f50450
Robert Collins <beau@collins.pub>
2018-03-07 11:14:38 -0800

    Uses es6 class syntax

--- src/simperium/queues/local-queue.js
+++ src/simperium/queues/local-queue.js
@@ -1,144 +1,145 @@
 // @flow
-import { inherits } from 'util';
 import events from 'events';
+import * as change_util from '../util/change';
 
 const { EventEmitter } = events;
 
-export default function LocalQueue( store ) {
-	this.store = store;
-	this.sent = {};
-	this.queues = {};
-	this.ready = false;
-}
-
-inherits( LocalQueue, EventEmitter );
-
-LocalQueue.prototype.start = function() {
-	var queueId;
-	this.ready = true;
-	for ( queueId in this.queues ) {
-		this.processQueue( queueId );
-	}
-}
-
-LocalQueue.prototype.pause = function() {
-	this.ready = false;
-};
-
-LocalQueue.prototype.acknowledge = function( change ) {
-	if ( this.sent[change.id] === change ) {
-		delete this.sent[change.id];
+export default class LocalQueue extends EventEmitter {
+	constructor( store ) {
+		super();
+		this.store = store;
+		this.sent = {};
+		this.queues = {};
+		this.ready = false;
 	}
 
-	this.processQueue( change.id );
-}
-
-LocalQueue.prototype.queue = function( change ) {
-	var queue = this.queues[change.id];
-
-	if ( !queue ) {
-		queue = [];
-		this.queues[change.id] = queue;
+	start() {
+		var queueId;
+		this.ready = true;
+		for ( queueId in this.queues ) {
+			this.processQueue( queueId );
+		}
 	}
 
-	queue.push( change );
+	pause() {
+		this.ready = false;
+	};
 
-	this.emit( 'queued', change.id, change, queue );
-
-	if ( !this.ready ) return;
-
-	this.processQueue( change.id );
-};
-
-LocalQueue.prototype.hasChanges = function() {
-	return Object.keys( this.queues ).length > 0;
-};
-
-LocalQueue.prototype.dequeueChangesFor = function( id ) {
-	var changes = [], sent = this.sent[id], queue = this.queues[id];
-
-	if ( sent ) {
-		delete this.sent[id];
-		changes.push( sent );
-	}
-
-	if ( queue ) {
-		delete this.queues[id];
-		changes = changes.concat( queue );
-	}
-
-	return changes;
-};
-
-LocalQueue.prototype.processQueue = function( id ) {
-	var queue = this.queues[id];
-	var compressAndSend = this.compressAndSend.bind( this, id );
-
-	// there is no queue, don't do anything
-	if ( !queue ) return;
-
-	// queue is empty, delete it from memory
-	if ( queue.length === 0 ) {
-		delete this.queues[id];
-		return;
-	}
-
-	// waiting for a previous sent change to get acknowledged
-	if ( this.sent[id] ) {
-		this.emit( 'wait', id );
-		return;
-	}
-
-	this.store.get( id ).then( compressAndSend );
-}
-
-LocalQueue.prototype.compressAndSend = function( id, ghost ) {
-	var changes = this.queues[id];
-	var change;
-	var target = ghost.data;
-	var c;
-	var type;
-
-	// a change was sent before we could compress and send
-	if ( this.sent[id] ) {
-		this.emit( 'wait', id );
-		return;
-	}
-
-	if ( changes.length === 1 ) {
-		change = changes.shift();
-		this.sent[id] = change;
-		this.emit( 'send', change );
-		return;
-	}
-
-	if ( changes.length > 1 && changes[0].type === change_util.type.REMOVE ) {
-		change = changes.shift();
-		changes.splice( 0, changes.length - 1 );
-		this.sent[id] = change;
-		this.emit( 'send', change );
-	}
-
-	while ( changes.length > 0 ) {
-		c = changes.shift();
-
-		if ( c.o === change_util.type.REMOVE ) {
-			changes.unshift( c );
-			break;
+	acknowledge( change ) {
+		if ( this.sent[change.id] === change ) {
+			delete this.sent[change.id];
 		}
 
-		target = change_util.apply( c.v, target );
+		this.processQueue( change.id );
 	}
 
-	type = target === null ? change_util.type.REMOVE : change_util.type.MODIFY;
-	change = change_util.buildChange( type, id, target, ghost );
+	queue( change ) {
+		var queue = this.queues[change.id];
 
-	this.sent[id] = change;
-	this.emit( 'send', change );
-}
+		if ( !queue ) {
+			queue = [];
+			this.queues[change.id] = queue;
+		}
 
-LocalQueue.prototype.resendSentChanges = function() {
-	for ( let ccid in this.sent ) {
-		this.emit( 'send', this.sent[ccid] )
+		queue.push( change );
+
+		this.emit( 'queued', change.id, change, queue );
+
+		if ( !this.ready ) return;
+
+		this.processQueue( change.id );
+	};
+
+	hasChanges() {
+		return Object.keys( this.queues ).length > 0;
+	};
+
+	dequeueChangesFor( id ) {
+		var changes = [], sent = this.sent[id], queue = this.queues[id];
+
+		if ( sent ) {
+			delete this.sent[id];
+			changes.push( sent );
+		}
+
+		if ( queue ) {
+			delete this.queues[id];
+			changes = changes.concat( queue );
+		}
+
+		return changes;
+	};
+
+	processQueue( id ) {
+		var queue = this.queues[id];
+		var compressAndSend = this.compressAndSend.bind( this, id );
+
+		// there is no queue, don't do anything
+		if ( !queue ) return;
+
+		// queue is empty, delete it from memory
+		if ( queue.length === 0 ) {
+			delete this.queues[id];
+			return;
+		}
+
+		// waiting for a previous sent change to get acknowledged
+		if ( this.sent[id] ) {
+			this.emit( 'wait', id );
+			return;
+		}
+
+		this.store.get( id ).then( compressAndSend );
+	}
+
+	compressAndSend( id, ghost ) {
+		var changes = this.queues[id];
+		var change;
+		var target = ghost.data;
+		var c;
+		var type;
+
+		// a change was sent before we could compress and send
+		if ( this.sent[id] ) {
+			this.emit( 'wait', id );
+			return;
+		}
+
+		if ( changes.length === 1 ) {
+			change = changes.shift();
+			this.sent[id] = change;
+			this.emit( 'send', change );
+			return;
+		}
+
+		if ( changes.length > 1 && changes[0].type === change_util.type.REMOVE ) {
+			change = changes.shift();
+			changes.splice( 0, changes.length - 1 );
+			this.sent[id] = change;
+			this.emit( 'send', change );
+		}
+
+		while ( changes.length > 0 ) {
+			c = changes.shift();
+
+			if ( c.o === change_util.type.REMOVE ) {
+				changes.unshift( c );
+				break;
+			}
+
+			target = change_util.apply( c.v, target );
+		}
+
+		type = target === null ? change_util.type.REMOVE : change_util.type.MODIFY;
+		change = change_util.buildChange( type, id, target, ghost );
+
+		this.sent[id] = change;
+		this.emit( 'send', change );
+	}
+
+	resendSentChanges() {
+		for ( let ccid in this.sent ) {
+			this.emit( 'send', this.sent[ccid] )
+		}
 	}
 }
--- src/simperium/queues/network-queue.js
+++ src/simperium/queues/network-queue.js
@@ -1,21 +1,23 @@
 // @flow
 import Queue from './queue';
 
-export default function NetworkQueue() {
-	this.queues = {};
-}
-
-NetworkQueue.prototype.queueFor = function( id ) {
-	const queues: { [string]: ?Queue } = this.queues;
-	let queue: ?Queue = queues[id];
-
-	if ( !queue ) {
-		queue = new Queue();
-		queue.on( 'finish', function() {
-			delete queues[id];
-		} );
-		queues[id] = queue;
+export default class NetworkQueue {
+	constructor() {
+		this.queues = {};
 	}
 
-	return queue;
-};
+	queueFor( id ) {
+		const queues: { [string]: ?Queue } = this.queues;
+		let queue: ?Queue = queues[id];
+
+		if ( !queue ) {
+			queue = new Queue();
+			queue.on( 'finish', function() {
+				delete queues[id];
+			} );
+			queues[id] = queue;
+		}
+
+		return queue;
+	}
+}
